f6230eab707acaa46be3931fd1ee683eecc76728
ritesh3105 <ritesh.d@amazatic.com>
2023-12-20 15:20:37 +0530

    Started with Express & NodeJS.

--- server/index.js
+++ server/index.js
@@ -2,36 +2,54 @@ const http = require("http");
 const fs = require("fs");
 // To parse url
 const url = require("url");
+const express = require("express");
 
-const myServer = http.createServer((req, res) => {
-  const log = `${Date.now()}: Request received for ${req.url} \n`;
+const app = express();
 
-  // To extract query params from URL as object we need to pass 'true' as 2nd parameter
-  const myUrl = url.parse(req.url, true);
-  console.log(myUrl);
-
-  fs.appendFile("./log.txt", log, (err, response) => {
-    switch (myUrl.pathname) {
-      case "/":
-        res.end("Hello from Home");
-        break;
-      case "/about":
-        const username = myUrl.query.name;
-        console.log(username);
-        res.end(`Hello, ${username}`);
-        break;
-      case "/contact":
-        res.end("Hello from Contact");
-        break;
-      case "/search":
-        const searchParam = myUrl.query.search;
-        res.end(`Search for ${searchParam}`);
-        break;
-      default:
-        res.end("404 Not Found");
-        break;
-    }
-  });
+app.get("/", (req, res) => {
+  return res.send("Hello from home page");
 });
 
-myServer.listen(8000, () => console.log("Server is listening on port 8000"));
+app.get("/about", (req, res) => {
+  return res.send(`Hello from about page. Hi, ${req.query.name}`);
+});
+
+app.get("/contact", (req, res) => {
+  return res.send("Hello from contact page");
+});
+
+// const myHandler = (req, res) => {
+//   const log = `${Date.now()}: Request received for ${req.url} \n`;
+
+//   // To extract query params from URL as object we need to pass 'true' as 2nd parameter
+//   const myUrl = url.parse(req.url, true);
+//   console.log(myUrl);
+
+//   fs.appendFile("./log.txt", log, (err, response) => {
+//     switch (myUrl.pathname) {
+//       case "/":
+//         res.end("Hello from Home");
+//         break;
+//       case "/about":
+//         const username = myUrl.query.name;
+//         console.log(username);
+//         res.end(`Hello, ${username}`);
+//         break;
+//       case "/contact":
+//         res.end("Hello from Contact");
+//         break;
+//       case "/search":
+//         const searchParam = myUrl.query.search;
+//         res.end(`Search for ${searchParam}`);
+//         break;
+//       default:
+//         res.end("404 Not Found");
+//         break;
+//     }
+//   });
+// };
+
+// const myServer = http.createServer(app);
+
+// myServer.listen(8000, () => console.log("Server is listening on port 8000"));
+app.listen(8000, () => console.log("Server is listening on port 8000"));
