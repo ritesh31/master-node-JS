596aa312453cf516fe4632aa8e21a4a50973a7e3
ritesh3105 <ritesh.d@amazatic.com>
2023-12-20 14:35:43 +0530

    Handling URL in NodeJS.

--- server/index.js
+++ server/index.js
@@ -1,19 +1,31 @@
 const http = require("http");
 const fs = require("fs");
+// To parse url
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
   const log = `${Date.now()}: Request received for ${req.url} \n`;
 
+  // To extract query params from URL as object we need to pass 'true' as 2nd parameter
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
+
   fs.appendFile("./log.txt", log, (err, response) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("Hello from Home");
         break;
       case "/about":
-        res.end("Hello from About");
+        const username = myUrl.query.name;
+        console.log(username);
+        res.end(`Hello, ${username}`);
         break;
       case "/contact":
         res.end("Hello from Contact");
+        break;
+      case "/search":
+        const searchParam = myUrl.query.search;
+        res.end(`Search for ${searchParam}`);
         break;
       default:
         res.end("404 Not Found");
